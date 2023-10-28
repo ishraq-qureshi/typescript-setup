8ac6bf9260a686548e094bdb10e386eafd01dcc4
IshraqQureshi <56018643+IshraqQureshi@users.noreply.github.com>
2023-10-28 15:44:13 +0500

    Change in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,10 @@ import { getFormData } from "./form";
 
 const form = document.getElementById('myForm') as HTMLFormElement;
 
+window.alert('Tesitng');
+
 form.addEventListener("submit", function(e: SubmitEvent){
     e.preventDefault();
     const data = getFormData(form);
     console.log(data);
 })
-debugger;
-const person: any = {}
-
-console.log(person.speak());
